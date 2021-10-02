5c6528b60fe2bb427d6164bf18fc7013b0699641
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-10-02 18:17:14 +0900

    20211002 3

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/Form.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
@@ -48,33 +49,37 @@ function Registration() {
   
 
   return (
-    <div className="App">
-      <h1>新規登録</h1>
-      <Link to ="/login">
-        ログイン
-      </Link>
+    <div className="formBody">
+      <h2>新規登録</h2>
       <form onSubmit={handleSubmit(handleOnSubmit)}>
         <input
+          placeholder="ユーザーネーム"
           {...register("name", {
             required: true,
             maxLength: 10,
             pattern: /[0-9a-zA-Z]/,
           })}
         />
-        {errors.name?.types?.required && "文字が入力されていません"}
-        {errors.name?.types?.maxLength && "10文字以上が入力されています"}
-        {errors.name?.types?.pattern && "英数字以外の文字が含まれています"}
+        <div className="error">
+          {errors.name?.types?.required && "ユーザーネームが入力されていません"}
+          {errors.name?.types?.maxLength && "10文字以上が入力されています"}
+          {errors.name?.types?.pattern && "英数字以外の文字が含まれています"}
+          </div>
         <input
+          placeholder="メールアドレス"
           {...register("email", {
             required: true,
             maxLength: 10,
             pattern: /[0-9a-zA-Z]/,
           })}
         />
-        {errors.email?.types?.required && "文字が入力されていません"}
-        {errors.email?.types?.maxLength && "10文字以上が入力されています"}
-        {errors.email?.types?.pattern && "英数字以外の文字が含まれています"}
+        <div className="error">
+          {errors.email?.types?.required && "メールアドレスが入力されていません"}
+          {errors.email?.types?.maxLength && "10文字以上が入力されています"}
+          {errors.email?.types?.pattern && "英数字以外の文字が含まれています"}
+        </div>
         <input
+          placeholder="パスワード"
           type="password"
           {...register("password", {
             required: true,
@@ -82,10 +87,15 @@ function Registration() {
             pattern: /[0-9a-zA-Z]/,
           })}
         />
-        {errors.password?.types?.required && "文字が入力されていません"}
-        {errors.password?.types?.maxLength && "30文字以上が入力されています"}
-        {errors.password?.types?.pattern && "英数字以外の文字が含まれています"}
-        <input type="submit" value="登録"/>
+        <div className="error">
+          {errors.password?.types?.required && "パスワードが入力されていません"}
+          {errors.password?.types?.maxLength && "30文字以上が入力されています"}
+          {errors.password?.types?.pattern && "英数字以外の文字が含まれています"}
+        </div>
+        <button type="submit">登録</button>
+        <a href="/login" className="forgot">
+          ログインはこちら
+        </a>
       </form>
     </div>
   );
--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/Form.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
@@ -47,23 +48,24 @@ function Signin() {
   }
 
   return (
-    <div className="App">
-      <h1>ログイン</h1>
-      <Link to ="/signup">
-        新規登録
-      </Link>
+    <div className="formBody">
+      <h2>ログイン</h2>
       <form onSubmit={handleSubmit(handleOnSubmit)}>
         <input
+          placeholder="メールアドレス"
           {...register("email", {
             required: true,
             maxLength: 10,
             pattern: /[0-9a-zA-Z]/,
           })}
         />
-        {errors.email?.types?.required && "文字が入力されていません"}
-        {errors.email?.types?.maxLength && "10文字以上が入力されています"}
-        {errors.email?.types?.pattern && "英数字以外の文字が含まれています"}
+        <div className="error">
+          {errors.email?.types?.required && "メールアドレスが入力されていません"}
+          {errors.email?.types?.maxLength && "10文字以上が入力されています"}
+          {errors.email?.types?.pattern && "英数字以外の文字が含まれています"}
+        </div>
         <input
+          placeholder="パスワード"
           type="password"
           {...register("password", {
             required: true,
@@ -71,10 +73,15 @@ function Signin() {
             pattern: /[0-9a-zA-Z]/,
           })}
         />
-        {errors.password?.types?.required && "文字が入力されていません"}
-        {errors.password?.types?.maxLength && "30文字以上が入力されています"}
-        {errors.password?.types?.pattern && "英数字以外の文字が含まれています"}
-        <input type="submit" value="ログイン"/>
+        <div className="error">
+          {errors.password?.types?.required && "パスワードが入力されていません"}
+          {errors.password?.types?.maxLength && "30文字以上が入力されています"}
+          {errors.password?.types?.pattern && "英数字以外の文字が含まれています"}
+        </div>
+        <button type="submit">ログイン</button>
+        <a href="/signup" className="forgot">
+          新規登録はこちら
+        </a>
       </form>
     </div>
   );
